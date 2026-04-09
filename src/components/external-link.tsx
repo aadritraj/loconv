18726ad51a2197e4c627dbcd5e3c91e4100b262f
aadritraj <176519217+aadritraj@users.noreply.github.com>
2026-04-09 12:40:54 +0200

    move link styles to component
    (oops i forgot)

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -6,7 +6,7 @@ export function ExternalLink({
 	children: React.ReactNode;
 }) {
 	return (
-		<a href={href} target="_blank" rel="noopener noreferrer">
+		<a href={href} className="link" target="_blank" rel="noopener noreferrer">
 			{children}
 		</a>
 	);
