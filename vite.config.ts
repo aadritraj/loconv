dbea720dea3d920d158641016594f17400523f3b
aadritraj <176519217+aadritraj@users.noreply.github.com>
2026-04-14 11:15:17 +0200

    add commit hash to footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { execSync } from 'child_process';
+
+const getCommitHash = () => {
+  if (process.env.CF_PAGES_COMMIT_SHA) {
+    return process.env.CF_PAGES_COMMIT_SHA;
+  };
+
+  try {
+    return execSync('git rev-parse --short HEAD').toString().trim();
+  } catch {
+    return 'unknown';
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __COMMIT_HASH__: JSON.stringify(getCommitHash()),
+  }
 })
